2bb723cbd690d6336d85e4d99a71800c0ed7b90a
ANATOLII ZLOBIN <59967619+AnatoliiZlobin@users.noreply.github.com>
2022-07-31 19:21:41 +0200

    0.1

--- siteblog/blog/admin.py
+++ siteblog/blog/admin.py
@@ -1,3 +1,32 @@
-from django.contrib import admin
 
-# Register your models here.
+from tkinter.tix import Tree
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from .models import *
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    save_as = True
+    save_on_top = True
+    list_display = ['id', 'title', 'slug', 'category', 'created_ad', 'get_photo']
+    list_display_links = ['id', 'title']
+    search_fields = ['title']
+    list_filter = ['category']
+    readonly_fields = ['views', 'created_ad', 'get_photo']
+    fields = ['title', 'slug', 'category', 'tags', 'content', 'photo', 'get_photo', 'views', 'created_ad',]
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="50">')
+        return '-'
+    get_photo.short_description = 'Photo'
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Post, PostAdmin)
